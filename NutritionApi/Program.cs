dff72cc267279a405d3a05c9dffe372c24aed15c
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2023-08-20 17:10:38 +0200

    [BUG] Fixed NutritionDatabaseSettings to not have null values when using environment variable

--- NutritionApi/Program.cs
+++ NutritionApi/Program.cs
@@ -4,11 +4,11 @@ using NutritionApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 var dbUrl = Environment.GetEnvironmentVariable("NUTRITIONDB_CONNECTIONSTRING", EnvironmentVariableTarget.Process);
-if (dbUrl != null)
+if (dbUrl == null)
 {
     builder.Services.Configure<NutritionDatabaseSettings>(options =>
     {
-        builder.Configuration.GetSection("NutritionDatabaseSettings");
+        builder.Configuration.GetSection("NutritionDatabaseSettings").Bind(options);
         options.ConnectionString = dbUrl;
     });
 }
