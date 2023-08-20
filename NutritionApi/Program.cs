bfc471b9bb3ecdfdd23d922e3c41a64eb77e669a
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2023-08-20 17:17:26 +0200

    [FIX] switched if statement

--- NutritionApi/Program.cs
+++ NutritionApi/Program.cs
@@ -4,7 +4,7 @@ using NutritionApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 var dbUrl = Environment.GetEnvironmentVariable("NUTRITIONDB_CONNECTIONSTRING", EnvironmentVariableTarget.Process);
-if (dbUrl == null)
+if (dbUrl != null)
 {
     builder.Services.Configure<NutritionDatabaseSettings>(options =>
     {
